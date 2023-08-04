73a108dadb3bf16dd6fde1aa3f0fd75715cbb56a
websolutionus <websolutionus1@gmail.com>
2023-08-04 20:45:52 +0600

    first assignment

---
+++ myflutter/lib/main.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  const a = 25, b = 6;
+
+  var addition, substriction, multiplication, division, modulus;
+
+  addition = a+b;
+  print(addition);
+
+  substriction = a-b;
+  print(substriction);
+
+  multiplication = a*b;
+  print(multiplication);
+
+  division = a/b;
+  print(division);
+
+  modulus = a%b;
+  print(modulus);
+
+}
